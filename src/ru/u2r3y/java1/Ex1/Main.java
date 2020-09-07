48a8b87311b8196b822c032988a6add1c777e067
Kristina <1u2r3y@mail.ru>
2020-09-07 10:35:03 +0300

    Init

---
+++ src/ru/u2r3y/java1/Ex1/Main.java
@@ -0,0 +1,27 @@
+package ru.u2r3y.java1.Ex1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6};
+        int k = 0;
+        for (int i = 0; i < array.length; i++) {
+            k += array[i];
+        }
+        System.out.println(k);
+        int j = 0;
+        int k1 = 0;
+        while (j < array.length) {
+            k1 += array[j];
+            j++;
+        }
+        System.out.println(k1);
+        int k2 = 0;
+        int l = 0;
+        do {
+            k2 += array[l];
+            l++;
+        } while (l < array.length);
+        System.out.println(k2);
+    }
+}
---
+++ src/ru/u2r3y/java1/Ex3/Main.java
@@ -0,0 +1,11 @@
+package ru.u2r3y.java1.Ex3;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i<11; i++){
+            double v = 1.0/i;
+            String m = String.format("%/2f",v);
+            System.out.println(m + " ");
+        }
+    }
+}
