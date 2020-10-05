b73ed1710cd378b02fdf9bfa95d292ccb9aadeca
1u2r3 <1u2r3y@mail.ru>
2020-10-05 15:29:32 +0300

    Pr10(1-8)

---
+++ src/ru/u2r3y/java1/Pr10/Ex2/Main.java
@@ -0,0 +1,13 @@
+package ru.u2r3y.java1.Pr10.Ex2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+        int k = scan.nextInt();
+        for (int j = 1; j <= k; j++){
+            System.out.print(j + " ");
+        }
+    }
+}
---
+++ src/ru/u2r3y/java1/Pr10/Ex3/Main.java
@@ -0,0 +1,20 @@
+package ru.u2r3y.java1.Pr10.Ex3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        if (A<B){
+            for (int i = A; i <= B; i++){
+                System.out.print(i + " ");
+            }
+        }else{
+            for (int i = A; i >= B; i--){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
