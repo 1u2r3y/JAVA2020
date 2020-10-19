47af9f6c37f395737ffdab2fffaa9387551f4b2d
1u2r3 <1u2r3y@mail.ru>
2020-10-19 11:01:33 +0300

    Pr13

---
+++ src/ru/u2r3y/java1/Pr13/SortingStudentsByGPA.java
@@ -0,0 +1,11 @@
+package ru.u2r3y.java1.Pr13;
+
+import java.util.Comparator;
+
+public class SortingStudentsByGPA implements Comparator <Student> {
+    @Override
+    public int compare(Student o1, Student o2) {
+        return  o1.getGpa()<o2.getGpa()? 1: -1;
+    }
+}
+
