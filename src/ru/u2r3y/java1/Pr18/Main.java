80817477beaadcbc99ef326e99e7f0e503afbeec
1u2r3 <1u2r3y@mail.ru>
2020-11-01 21:21:03 +0300

    Pr17 + Pr18

---
+++ src/ru/u2r3y/java1/Pr18/Main.java
@@ -0,0 +1,8 @@
+package ru.u2r3y.java1.Pr18;
+
+public class Main {
+    public static void main(String[] args) {
+        Exception2 main = new Exception2();
+        main.exceptionDemo();
+    }
+}
